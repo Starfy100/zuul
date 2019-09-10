2014e35608880b74bbdafcd91fd2426e266627b0
U-BSD\414786 <414786@SHS-10DEC49DF.beaverton.k12.or.us>
2019-09-10 08:48:42 -0700

    guessing game

--- guess.cpp
+++ guess.cpp
@@ -7,51 +7,63 @@
 using namespace std;
 
 int game = 1; //keeps track of game being on or off
-int input;
+int input = 0;
 int main()
 
 {
   
-  // int game = 1;
   int guess = 0; //keeps track of guesses  
 
  srand(time(NULL));//sets the random seed
- int random = rand()%100; //generates random number
   
   
-   while (game == 1) {
-      guess++;
+  while (game == 1) {
+    int random = rand()%100; //generates the random number
+    guess = 0;
 
-      cout << random << endl;
+    while (input != random) {
+      guess++; //increases the guesses counter
+
+      // cout << random << endl;
       // cout << "game in progress";
       // cout << guess;
       cin >> input;
-      
-
-      
-       if (input == random) { //win condition
-	cout << "you guessed right!" << endl;
-	cout << "it took you: " << guess << " tries" << endl;
-	game = 0;
 
 
-	if (game = 0) {
-	  cout << "do you want to play again?"
-	  //ask to play again
-	}
-       }
       
        if (input > random) { //if your guess is too high... 
 	  cout << "too high!" << endl;
 	}
        if (input < random) { //if your guess is too low...
 	  cout << "too low!" << endl;
-	}
-      
+	}    
 
     }
-  
-  return 0;
+
+
+    //if the guess is correct, ask to play again
+	cout << "you guessed right!" << endl;
+	cout << "it took you: " << guess << " tries" << endl;
+ 
+	
+       cout << "do you want to play again? y/n"<< endl;
+	  //asks to play again, if yes: restart/if no: end
+       cin >> input;
+       cout << input << endl;
+
+
+       if (input == 1){
+	
+	  // int random = rand()%100;
+	 // cout << random << endl;
+	 }
+
+       if (input == 0) {
+	 game = 0;
+       }
+
+    
+  }
   
 }
 
