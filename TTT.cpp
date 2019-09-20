3397a3483b7238ba6fb08e4446f7430818675bb5
U-BSD\414786 <414786@SHS-10DEC49DF.beaverton.k12.or.us>
2019-09-20 09:03:38 -0700

    TTT

---
+++ TTT.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+/*
+Author: Nicolas F.
+Tic Tac Toe: Program will set up a 3x3 board for two players to play Tic Tac Toe
+If a player wins or ties, the board will be reset and scores displayed
+
+Start Date: 9/20/19
+ */
+using namespace std;
+
+int main() {
+
+  int board[3][3];
+  int xwin = 0;
+  int ywin = 0;
+  bool gamestart = true;
+
+
+  while (gamestart == true) {
+
+    cout << "Select row (1,2 or 3)" << endl; 
+    cin.get;
+    
+  
+
+
+    
+
+  }
+
+
+
+  
+
+  return 0;
+}
+
