3a499cb86775f681ed25ec26d1fa4004eab567da
U-BSD\414786 <414786@SHS-10DEC49DF.beaverton.k12.or.us>
2019-09-30 09:09:20 -0700

    list

---
+++ list.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+/*
+Author: Nicolas F.
+Student List:
+
+
+ */
+
+using namespace std;
+struct Student {
+  char firstname[10];
+  char lastname[10];
+  int id;
+  float gpa;
+};
+
+void add() {
+
+}
+
+void del() {
+
+}
+
+void print() {
+
+}
+
+
+
+int main() {
+  cout << "Test!" << endl;
+
+  return 0;
+}
+
